f21170c0278bfc9f5f43def3b30284fd40133ca8
stuehmer <roland.stuehmer@fzi.de>
2012-11-08 13:08:57 +0100

    Create a dynamic URI on each start to receive events avoiding events from old subscriptions.

--- app/models/jobs/Bootstrap.java
+++ app/models/jobs/Bootstrap.java
@@ -1,11 +1,15 @@
 package models.jobs;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import models.ModelManager;
 import models.User;
 import play.Logger;
 import play.jobs.*;
+import play.mvc.Router;
 
 /**
  * Bootstrap commands on application start
@@ -18,12 +22,22 @@ public class Bootstrap extends Job {
     
     public void doJob() {
     	Logger.info("- Application Start -");
-        ModelManager.get();
+
+		// Generate dynamic route for this application lifetime to avoid receiving events from old subscriptions:
+		Router.prependRoute("POST", "/webservice/soapnotifendpoint" + Math.abs(new Random().nextLong()) + "/{topicId}", "WebService.soapNotifEndPoint", "", "");
+		Map params = new HashMap<String, Object>();
+		params.put("topicId", "someTopicId");
+		String notificationsEndPoint = Router.getFullUrl("WebService.soapNotifEndPoint",
+				params);
+		Logger.info("Generated dynamic route to avoid old events: " + notificationsEndPoint);
+
+    	ModelManager.get();
 		List<User> users = User.findAll();
 		for (User u : users) {
 			u.connected = 0;
 			u.save();
 		}
+
         Logger.info("- Bootstrap executed -");
     }
 }
